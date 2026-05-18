4708fbea600ccd9938b2b754bbbc8a135b822450
BenRaz123 <ben.raz2008@gmail.com>
2026-05-18 14:16:03 +0000

    changed qutebrowser config
    Signed-off-by: BenRaz123 <ben.raz2008@gmail.com>

--- home/ben/modules/qutebrowser.nix
+++ home/ben/modules/qutebrowser.nix
@@ -13,20 +13,21 @@ in
       pkgs = "https://search.nixos.org/packages?channel=unstable&query={}";
     };
     settings = {
+      url.default_page = "https://google.com";
       fonts.default_family = "${font.name} Light";
       fonts.default_size = "13pt";
-      #content.user_stylesheets = pkgs.writeText "qute-stylesheet.css" ''
-      #  *:not(
-      #    i, i *,
-      #    pre, pre *,
-      #    code, code *,
-      #    textarea, textarea *,
-      #    .editor, .editor *
-      #  ) {
-      #    font-family: "${font.name}" !IMPORTANT;
-      #    border-radius: 0 !IMPORTANT;
-      #  }
-      #'';
+      content.user_stylesheets = "${pkgs.writeText "qute-stylesheet.css" ''
+        *:not(
+          i, i *,
+          pre, pre *,
+          code, code *,
+          textarea, textarea *,
+          .editor, .editor *
+        ) {
+          font-family: "${font.name}" !IMPORTANT;
+          border-radius: 0 !IMPORTANT;
+        }
+      ''}";
     };
   };
 }
