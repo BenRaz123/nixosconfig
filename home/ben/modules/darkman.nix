870f87167bf9457b2e8e5da86a47f8a16461ce29
BenRaz123 <ben.raz2008@gmail.com>
2026-06-15 03:42:26 +0000

    added support for theming gtk3 apps
    Signed-off-by: BenRaz123 <ben.raz2008@gmail.com>

--- home/ben/modules/darkman.nix
+++ home/ben/modules/darkman.nix
@@ -5,10 +5,12 @@
     lightModeScripts.gtk-theme = ''
       ${pkgs.dconf}/bin/dconf write \
           /org/gnome/desktop/interface/color-scheme "'prefer-light'"
+      ${pkgs.glib}/bin/gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-light" # for gtk3 apps
     '';
     darkModeScripts.gtk-theme = ''
       ${pkgs.dconf}/bin/dconf write \
           /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+      ${pkgs.glib}/bin/gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark" # for gtk3 apps
     '';
     settings = {
       usegeoclue = true;
